9b3beb802616d02adbddca37c6a98b7f22fc0837
Jintime <wlstjd7391@gmail.com>
2023-05-08 17:34:59 +0900

    수정

--- src/main/java/com/oncha/oncha_web/feature/MainController.java
+++ src/main/java/com/oncha/oncha_web/feature/MainController.java
@@ -1,8 +1,20 @@
 package com.oncha.oncha_web.feature;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @GetMapping("/index")
+    public String Home(){return "index_test";}
+    @GetMapping("/finder")
+    public String TeaFinder(){return "category/tea_finder";}
+    @GetMapping("/store")
+    public String Store(){return "category/store";}
+    @GetMapping("/reservation")
+    public String Reservation(){return "category/reservation";}
+    @GetMapping("/subscribe")
+    public String Subscribe(){return "/category/subscribe";}
 
 }
