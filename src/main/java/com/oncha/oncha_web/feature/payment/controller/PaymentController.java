22bff0385396d5f49f30d58f81b5a006592f6783
Jintime <wlstjd7391@gmail.com>
2023-05-17 01:25:59 +0900

    슈정

--- src/main/java/com/oncha/oncha_web/feature/payment/controller/PaymentController.java
+++ src/main/java/com/oncha/oncha_web/feature/payment/controller/PaymentController.java
@@ -1,14 +1,40 @@
 package com.oncha.oncha_web.feature.payment.controller;
 
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.Locale;
 
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/order")
 public class PaymentController {
 
+    private IamportClient api;
 
+    public PaymentController() {
+        // REST API 키와 REST API secret 를 아래처럼 순서대로 입력한다.
+        this.api = new IamportClient("3452420157053319","DXI0XVlgpUwS8B3Hj9cFxozTrGn6CXmfU4fS0B8pvFhDglkzvxe3VNXnukt7hPmcUC4UddkJSCn9XIJt");
+    }
+
+    @ResponseBody
+    @RequestMapping(value="/verifyIamport/{imp_uid}")
+    public IamportResponse<Payment> paymentByImpUid(
+            Model model
+            , Locale locale
+            , HttpSession session
+            , @PathVariable(value= "imp_uid") String imp_uid) throws IamportResponseException, IOException
+    {
+        return api.paymentByImpUid(imp_uid);
+    }
 
 }
