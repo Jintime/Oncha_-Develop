84b85f2ba1b59e034ff1df0b01e84e67c66e6830
jin <wlstjd7391@gmail.com>
2023-05-24 11:21:40 +0900

    [feature/#29] 로직 및 기능수정

--- src/main/java/com/oncha/oncha_web/feature/MainController.java
+++ src/main/java/com/oncha/oncha_web/feature/MainController.java
@@ -1,15 +1,27 @@
 package com.oncha.oncha_web.feature;
 
 import com.oncha.oncha_web.feature.productBoard.model.ProductBoardDTO;
+import com.oncha.oncha_web.feature.productBoard.model.ProductBoardRequest;
+import com.oncha.oncha_web.feature.productBoard.service.ProductBoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final ProductBoardService productBoardService;
+
     @GetMapping("/index")
-    public String Home(){return "index";}
+    public String Home(Model model){
+       // List<ProductBoardDTO> productDTOList = productBoardService.findAll();
+        //model.addAttribute("productList",productDTOList);
+        return "index";}
 
     @GetMapping("/login")
     public String Login(){return "login/loginForm";}
