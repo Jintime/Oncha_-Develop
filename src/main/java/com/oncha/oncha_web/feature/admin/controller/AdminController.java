664859ec028196e9aa3c19132c0fe2fda9c306fb
jin <wlstjd7391@gmail.com>
2023-05-24 18:25:44 +0900

    [feature/#29] 수정필요

--- src/main/java/com/oncha/oncha_web/feature/admin/controller/AdminController.java
+++ src/main/java/com/oncha/oncha_web/feature/admin/controller/AdminController.java
@@ -1,7 +1,6 @@
-package com.oncha.oncha_web.feature;
+package com.oncha.oncha_web.feature.admin.controller;
 
 import com.oncha.oncha_web.feature.productBoard.model.ProductBoardDTO;
-import com.oncha.oncha_web.feature.productBoard.model.ProductBoardRequest;
 import com.oncha.oncha_web.feature.productBoard.service.ProductBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -9,24 +8,21 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public class MainController {
+@RequestMapping("/on")
+public class AdminController {
 
     private final ProductBoardService productBoardService;
-
-    @GetMapping("/")
-    public String Home(Model model,@PageableDefault Pageable pageable){
-       List<ProductBoardDTO> productDTOList = productBoardService.findAll(pageable);
-        model.addAttribute("product",productDTOList);
-        return "index";}
-
-    @GetMapping("/login")
-    public String Login(){return "login/loginForm";}
-
+    @GetMapping("list")
+    public String viewProductList(Model model, @PageableDefault Pageable pageable){
+        List<ProductBoardDTO> productBoardDTOList = productBoardService.findAll(pageable);
+        model.addAttribute("productList",productBoardDTOList);
+        return "admin/adminProductList";
+    }
 
 }
