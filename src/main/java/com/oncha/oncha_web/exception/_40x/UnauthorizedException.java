daea8a898328d07134134d56d90410d14b13a473
applet <celbever@naver.com>
2023-06-09 13:06:17 +0900

    [feature/#40] 남은 exception 등록

---
+++ src/main/java/com/oncha/oncha_web/exception/_40x/UnauthorizedException.java
@@ -0,0 +1,10 @@
+package com.oncha.oncha_web.exception._40x;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException() {}
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
