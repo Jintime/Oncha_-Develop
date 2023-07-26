a7feef15daccf3d6fe8223962a0e8b06466c8371
jin <wlstjd7391@gmail.com>
2023-07-26 18:10:05 +0900

    [feature/#78] 프론트 수정

--- src/main/resources/static/js/filter.js
+++ src/main/resources/static/js/filter.js
@@ -10,12 +10,8 @@ function applyFilters() {
             selectedData[key] = value;
         }
     });
-
-    // 결과 출력 또는 백엔드로 데이터 전송
     console.log(selectedData);
-    // 백엔드로 데이터를 전송하려면 AJAX 등의 방법을 이용하여 서버에 데이터를 전송하면 됩니다.
-    // 예를 들면, fetch API를 이용하여 POST 요청을 보낼 수 있습니다.
-    // fetch('/backend', {
+    // fetch('/filter', {
     //     method: 'POST',
     //     headers: {
     //         'Content-Type': 'application/json'
@@ -31,7 +27,6 @@ function applyFilters() {
     // });
 }
 
-// 이벤트 리스너 등록
 document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
     checkbox.addEventListener('change', applyFilters);
 });
