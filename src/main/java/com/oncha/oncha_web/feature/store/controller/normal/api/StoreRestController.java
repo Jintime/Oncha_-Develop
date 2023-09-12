d0be3ee7bff5669c9b515538cdd39ac57b68dd2e
jin <wlstjd7391@gmail.com>
2023-09-12 18:18:53 +0900

    [feature/#92] 프론트 수정

--- src/main/java/com/oncha/oncha_web/feature/store/controller/normal/api/StoreRestController.java
+++ src/main/java/com/oncha/oncha_web/feature/store/controller/normal/api/StoreRestController.java
@@ -1,7 +1,6 @@
 package com.oncha.oncha_web.feature.store.controller.normal.api;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StoreRestController {
